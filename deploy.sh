bd8f65cd5fdb4481926499e5362c93f19d66046e
JackEMiller <jmiller@academytrainee.com>
2021-04-11 22:11:41 +0000

    env

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ echo $DATABASEURI >> databaseuri.txt
 cat databaseuri.txt
 scp databaseuri.txt jenkins@docker-master:databaseuri.txt
 ssh jenkins@docker-master << EOF
+    export 
     sudo docker login --username=$DOCKERHUB_USR --password=$DOCKERHUB_PSW
     sudo docker pull jmiller2612/prizepipeline_service1:1
     sudo docker pull jmiller2612/prizepipeline_service2:1
