746d938d4a0effb8ba001e36daf8d04669b7adcf
JackEMiller <jmiller@academytrainee.com>
2021-04-11 12:47:24 +0000

    jenkins update

--- tests/test_s1.py
+++ tests/test_s1.py
@@ -1,13 +1,28 @@
 from flask_testing import TestCase
 from flask import url_for
 from flask import Flask
-from service1 import app
+from service1 import app, db, Account
+import os
 
 
 class TestBase(TestCase):
 
     def create_app(self):
+        app.config.update(SQLALCHEMY_DATABASE_URI=os.getenv('DATABASEURI'),
+                SECRET_KEY='TEST_SECRET_KEY',
+                DEBUG=True,
+                WTF_CSRF_ENABLED=False)
         return app.app
+    
+    def setUp(self):
+        db.create_all()
+        sampleuser = Users(account_string="aaaaa",account_int="11111",prize=10)
+        db.session.add(sampleuser)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
 
 class TestViews(TestBase):
