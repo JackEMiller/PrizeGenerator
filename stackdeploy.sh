63fe0005876f2cc1fb4acfba4d6dd7f3457c3b17
JackEMiller <jmiller@academytrainee.com>
2021-04-11 16:34:18 +0000

    key

--- stackdeploy.sh
+++ stackdeploy.sh
@@ -4,4 +4,4 @@ sudo docker pull jmiller2612/prizepipeline_service1:1
 sudo docker pull jmiller2612/prizepipeline_service2:1
 sudo docker pull jmiller2612/prizepipeline_service3:1
 sudo docker pull jmiller2612/prizepipeline_service4:1
-sudo docker stack deploy --compose-file dockerstack-compose.yaml prizegeneratorstack
+sudo docker stack deploy --compose-file docker-compose.yaml prizegeneratorstack
