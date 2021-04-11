5f9cabecc249a7515d632582de5dff23871cb3b3
JackEMiller <jmiller@academytrainee.com>
2021-04-11 13:08:17 +0000

    jenkins update

--- tests/test_s1.py
+++ tests/test_s1.py
@@ -1,14 +1,14 @@
 from flask_testing import TestCase
 from flask import url_for
 from flask import Flask
-from service1 import app
+import service1
 import os
 
 
 class TestBase(TestCase):
 
     def create_app(self):
-        app.app.config.update(SQLALCHEMY_DATABASE_URI=os.getenv('DATABASEURI'),
+        service1.app.app.config.update(SQLALCHEMY_DATABASE_URI=os.getenv('DATABASEURI'),
                 SECRET_KEY='TEST_SECRET_KEY',
                 DEBUG=True,
                 WTF_CSRF_ENABLED=False)
